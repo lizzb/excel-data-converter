516ba50972b20c7d34d8671e94392851a179209c
lizzb <huzzah.its.lizz@gmail.com>
2014-07-01 16:06:33 -0700

    moving shiz around

--- js/dataConverter.js
+++ js/dataConverter.js
@@ -11,7 +11,6 @@ var dataTypes = {
 
 
 
-
 //-----------------------------------------------------------------------------
 //
 // Data Conversion Settings
@@ -96,20 +95,25 @@ var outputBooths = document.getElementById('dataOutput-booths'); //.value;
 
 function convertDataMajors() {
 
-  "dataInput-"
-  "dataOutput-"
-  "majors" "positions" "booths"
+  //"dataInput-"
+  //"dataOutput-"
+  //"majors" "positions" "booths"
 
+  // textarea input element and output element ids
+  var inputTextArea          = document.getElementById('dataInput-majors');
+  var outputTextArea         = document.getElementById('dataOutput-majors');
+  convertData(inputTextArea, outputTextArea);
   
-
 } // end convert majors
-
 
 
 
 function convertDataPositions() {
 
-
+  // textarea input element and output element ids
+  var inputTextArea          = document.getElementById('dataInput-positions');
+  var outputTextArea         = document.getElementById('dataOutput-positions');
+  convertData(inputTextArea, outputTextArea);
 
 } // end convert positions
 
@@ -117,7 +121,10 @@ function convertDataPositions() {
 
 function convertDataBooths() {
 
-
+  // textarea input element and output element ids
+  var inputTextArea          = document.getElementById('dataInput-booths');
+  var outputTextArea         = document.getElementById('dataOutput-booths');
+  convertData(inputTextArea, outputTextArea);
 
 } // end convert booths
 
@@ -141,13 +148,16 @@ function convertData(inputTextArea, outputTextArea) {
   var columnDelimiter        = "\t";
   var rowDelimiter           = "\n";
 
+  // Fed in through parameters - element ids of textarea tags
   //var inputTextArea          = {};
   //var outputTextArea         = {};
 
+  // Hardcoded in HTML now
   //var inputHeader            = {};
   //var outputHeader           = {};
-  //var dataSelect             = {};
+  //var dataSelect             = {}; // removed
 
+  // not used?? ....
   //var commentLine            = "//";
   //var commentLineEnd         = "";
  
@@ -166,19 +176,25 @@ function convertData(inputTextArea, outputTextArea) {
   var delimiter              = "auto";
 
 
-  // textarea input element id
-  var inputTextArea          = document.getElementById('dataInput-positions');
-  var outputTextArea         = document.getElementById('dataOutput-positions');
 
-    var inputText = inputTextArea.value;
-    //var outputText = "";
+  // Get the current value (raw data) in the textarea tag
+  // the element was fetched by id and fed in as a parameter using vanilla js
+  var inputText = inputTextArea.value;
 
   // make sure there is input data before converting...
   if (inputText.length > 0) {
 
+    //
+    // global??
+    //
     if (includeWhiteSpace == true) {  newLine = "\n";  }
     else {  indent = "";  newLine = "";  }
 
+
+
+    //
+    //  ******* Actual data conversion happens *******
+    //
 
     CSVParser.resetLog();
     var parseOutput = CSVParser.parse(inputText, headersProvided, delimiter, downcaseHeaders, upcaseHeaders);
@@ -188,8 +204,11 @@ function convertData(inputTextArea, outputTextArea) {
     var headerTypes = parseOutput.headerTypes;
     var errors = parseOutput.errors;
 
+    // Get the newly formatted data to put in output textarea element
     // var
     var outputText = renderData(dataGrid, headerNames, headerTypes, indent, newLine, propertyQuotes);
+
+    outputTextArea.value = errors + outputText;
 
     //this.outputTextArea.val(errors + this.outputText);    //jquery
     //this.outputTextArea = document.getElementById('dataOutput');
@@ -198,14 +217,48 @@ function convertData(inputTextArea, outputTextArea) {
     //document.getElementById('dataOutput-majors').value = errors + outputText;
     //document.getElementById('dataOutput-positions').value = errors + outputText;
 
-    outputTextArea.value = errors + outputText;
-
-
   }; //end test for existence of input text
 } // end convert data
 
 
+/*
+function convertData(dataType) {
 
+  switch(dataType)
+  {
+    case MAJORS:
+      convertDataMajors();
+      break;
+    case POSITIONS:
+      convertDataPositions();
+      break;
+    case BOOTHS:
+      convertDataBooths();
+      break;
+    case COMPANIES:
+      convertDataCompanies();
+      break;
+    default:
+      break;
+  }
+
+  switch(dataType)
+  {
+    case "MAJORS":
+      convertDataMajors();
+      break;
+    case "POSITIONS":
+      convertDataPositions();
+      break;
+    case "BOOTHS":
+      convertDataBooths();
+      break;
+    case "COMPANIES":
+      convertDataCompanies();
+      break;
+    default:
+      break;
+  }*/
 
 
 
@@ -288,3 +341,223 @@ function renderData(dataGrid, headerNames, headerTypes, indent, newLine, propQuo
 
   //$("#dataSelector").bind('change',function(evt){ self.convert(); });
 */
+
+
+
+
+//-----------------------------------------------------------------------------
+//
+//   ON PAGE LOAD
+//
+//-----------------------------------------------------------------------------
+/*$(document).ready(function(){
+
+  // hacky, temporary
+  // hacky, temporary
+  // hacky, temporary
+  //$("#dataInput-majors").keyup(function()   { self.convert(); });
+  //$("#dataInput-majors").change(function()  { self.convert(); });
+
+  $("#dataInput-majors").keyup(function()   { convertDataMajors(); });
+  $("#dataInput-majors").change(function()  { convertDataMajors(); });
+
+
+  $("#dataInput-positions").keyup(function()  { convertDataPositions(); });
+  $("#dataInput-positions").change(function() { convertDataPositions(); });
+
+  $("#dataInput-booths").keyup(function()   { convertDataBooths(); });
+  $("#dataInput-booths").change(function()  { convertDataBooths(); });
+*/
+
+  //var sidebar = $('#header');
+
+  //var widthOffset = 345;
+  //var heightOffset = 35
+
+  //var win = $(window);
+  //var w = win.width() - widthOffset;
+  //var h = win.height() - heightOffset;
+
+  //var d = new DataConverter('converter');
+  //d.create(); //(w,h);
+  //d.convert(); // want this to go instantly... grr.......TODO
+
+  //$(".settingsElement").change(updateSettings);
+
+
+
+  
+
+  
+
+  // hacky, temporary
+  // http://gomakethings.com/ditching-jquery-for-vanilla-js/
+  // want to get AWAY form jquery, use vanilla js
+  /*
+  var dataInputs = document.querySelectorAll('.dataInput');
+
+  [].forEach.call(dataInputs, function (rawdata) {
+    rawdata.addEventListener('click', function(e) {  // Do stuff
+  }, false);
+
+    rawdata.addEventListener('keyup', function(e) { self.convert(); }, false);
+    rawdata.addEventListener('change', function(e) { self.convert(); }, false);
+*/
+
+
+
+   /* if ( 'querySelector' in document && 'addEventListener' in window && Array.prototype.forEach ) {
+    document.documentElement.className += 'js';
+}*/
+
+
+/*
+// Sandwich Functions
+if ( 'querySelector' in document && 'addEventListener' in window ) {
+
+    var sandwiches = document.querySelectorAll('.sandwich');
+
+    [].forEach.call(sandwiches, function (sandwich) {
+
+        sandwich.addEventListener('click', function(e) {
+
+            if ( hasClass(sandwich, 'mustard') ) {
+                removeClass(sandwich, 'mustard');
+            }
+
+            else {
+                addClass(sandwich, 'mustard');
+            }
+        }
+        
+    });
+*/
+//}
+
+
+  
+//}) // DOCUMENT READY
+
+
+
+
+  //---------------------------------------
+  // PUBLIC PROPERTIES
+  //---------------------------------------
+
+  //this.nodeId                 = nodeId;
+  //this.node                   = $("#"+nodeId);
+
+      //
+    // WHITESPACE
+    //
+    //d.includeWhiteSpace = $('#includeWhiteSpaceCB').attr('checked');
+    /*
+    if (d.includeWhiteSpace)
+    {
+      $("input[name=indentType]").removeAttr("disabled");
+      var indentType = $('input[name=indentType]:checked').val();
+      if (indentType === "tabs") {  d.indent = "\t";  } 
+      else if (indentType === "spaces") {  d.indent = "  "  }
+    } 
+    else {  $("input[name=indentType]").attr("disabled", "disabled");  }
+    */
+
+  /*    
+    //
+    // HEADERS
+    //
+    d.headersProvided = $('#headersProvidedCB').attr('checked');
+
+    if (d.headersProvided) {
+      $("input[name=headerModifications]").removeAttr("disabled");
+
+      var hm = $('input[name=headerModifications]:checked').val();
+*/
+
+  /*
+  if (hm === "downcase")
+        d.downcaseHeaders = true; d.upcaseHeaders = false;
+
+      else if (hm === "upcase")
+        d.downcaseHeaders = false; d.upcaseHeaders = true;
+
+      else if (hm === "none")
+        d.downcaseHeaders = false; d.upcaseHeaders = false;
+
+    else {  $("input[name=headerModifications]").attr("disabled", "disabled");  }
+  */
+  
+    //d.delimiter = $('input[name=delimiter]:checked').val();
+  //d.decimal = $('input[name=decimal]:checked').val();
+  
+
+/*
+SETTINGS
+
+Delimiter:   Auto   Comma   Tab
+Decimal Sign:   Dot   Comma
+ First row is the header
+Transform:  downcase   upcase   none
+ Include white space in output
+Indent with:  tabs  spaces
+*/
+
+//-----------------------------------------------------------------------------
+  // ----- ----- ----- SETTINGS ----- ----- -----
+  // ----- SETTINGS -----
+/*
+  // Delimiter:   Auto   Comma   Tab
+  //
+  var columnDelimiter        = "\t";
+  var rowDelimiter           = "\n";
+
+  
+  // Decimal Sign:   Dot   Comma
+  //
+  
+  
+  // First row is the header (true/false)
+  //
+  var headersProvided        = true;
+  
+
+  // Transform:  downcase   upcase   none
+  //
+  var downcaseHeaders        = true; 
+  var upcaseHeaders          = false; // can't be true if downcase = true
+  
+
+  // Include white space in output (true/false)
+  //
+  var includeWhiteSpace      = true;
+  var useTabsForIndent       = false; // can't be true if whitespace = true
+
+
+  // Indent with:  tabs  spaces
+  //
+  var newLine                = "\n";
+  var indent                 = "  ";
+  
+  
+  /*if (includeWhiteSpace == true)  {  newLine = "\n";  }
+  else {    indent = "";  newLine = "";  }*/
+
+
+  // .............................
+  //
+ // var propertyQuotes = true;
+
+
+//-----------------------------------------------------------------------------
+
+
+
+
+//-----------------------------------------------------------------------------
+
+
+
+
+
+//-----------------------------------------------------------------------------
